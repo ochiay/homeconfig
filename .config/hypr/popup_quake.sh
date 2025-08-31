4d6f9eb04072bdb3ee74e28ee536a4f74df526da
Vladimir Stetciuk <imoen@disroot.org>
2025-08-31 21:11:36 +0300

    hyprland:   exec-once commands moved to autostart.sh   term was tied with special workspace   modshift+comma toggle special workspace fm(but there is no fm)   unfocused floating tagged(hidefloat tag) windows are transparent
    .diery.org:
      django:
        lookups tip
        settings tip
      vscode:
        settings tip for python
      postgres:
        tip for initializing db

--- .config/hypr/popup_quake.sh
+++ .config/hypr/popup_quake.sh
@@ -1,18 +1,29 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 popup=$1
 
-# Найдём адрес первого окна с классом "quake"
+# Получаем ID текущего рабочего стола
+current_ws=$(hyprctl activeworkspace -j | jq '.id')
 
-
-win=$(hyprctl clients | awk '
-    /Window/ { addr = $2 }
-    /class:[ \t]*quake/ { print addr }
-' | head -n 1)
-
-if [ -n "$win" ]; then
-    hyprctl dispatch killwindow "address:0x$win"
-else
-    # Иначе — запускаем новое
+# Ищем первое окно с классом "quake"
+win=$(hyprctl clients -j | jq -r '.[] | select(.class == "quake") | .address' | head -n 1)
+    
+if [ -z "$win" ]; then
     $popup --class quake
+    exit 0
 fi
+
+# Узнаём, на каком WS окно сейчас
+
+win_ws=$(hyprctl clients -j | jq -r --arg addr "$win" '.[] | select(.address==$addr) | .workspace.id')
+
+if [ "$win_ws" -eq "$current_ws" ]; then
+    # Окно на текущем WS — убиваем/сворачиваем
+    hyprctl dispatch movetoworkspacesilent special:quake,"address:$win"
+    
+else
+    # Окно есть, но на другом WS — переносим на текущий и фокусируем
+    hyprctl dispatch movetoworkspace "$current_ws","address:$win"
+    hyprctl dispatch focuswindow "address:$win"
+fi
+
